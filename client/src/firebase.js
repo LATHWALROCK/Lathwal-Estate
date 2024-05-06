73018aaad918e12eb302fb34376d14d2ffad0772
LATHWALROCK <sourabhlathwal1@gmail.com>
2024-05-06 16:57:59 +0530

    hello

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "lathwal-estate.firebaseapp.com",
+  projectId: "lathwal-estate",
+  storageBucket: "lathwal-estate.appspot.com",
+  messagingSenderId: "1047080216461",
+  appId: "1:1047080216461:web:5c61595421024a8b687fc4"
 };
 
 // Initialize Firebase
